d3bfb663b44d60435b54bf0866f49a34c2a9c830
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-27 11:18:25 +0530

    update:middleware

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
 
-// req => middleware => res
+let { people } = require("./data");
 
-// app.use("/api", logger); // app.use() will invoke logger function for any route
-// note : order matters
-
-// if path is provided--- middleware function will be called according to give path
-
-// to pass multiple middleware, use array
-app.use([logger, authorize]); // these will be execute in order
-
-app.get("/", (req, res) => {
-  res.send("Home");
-});
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-app.get("/api/items", (req, res) => {
-  console.log(req.user);
-  res.send("Items");
+app.get("/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
 app.listen(8000, () => {
